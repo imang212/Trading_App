fced6999ca0306c78a8e061a0b959914ae0b4dd0
imang212 <imang.gamer@gmail.com>
2026-06-03 11:14:55 +0200

    App logo add

--- app/src/main/java/com/trading/trading_app/model/AppConfig.java
+++ app/src/main/java/com/trading/trading_app/model/AppConfig.java
@@ -49,17 +49,32 @@ public final class AppConfig {
     public static final Map<String, String> ASSET_PROFILES;
     static {LinkedHashMap<String, String> a = new LinkedHashMap<>();
         // Commodities & Futures
-        a.put("Gold", "GC=F"); a.put("Silver", "SI=F"); a.put("Oil", "CL=F"); a.put("USDIDX", "DX-Y.NYB"); a.put("Brent_Oil", "BZ=F"); a.put("NATGAS", "HH=F");
+        a.put("Gold", "GC=F"); a.put("Silver", "SI=F"); a.put("Oil", "CL=F"); 
+        a.put("USDIDX", "DX-Y.NYB"); a.put("Brent_Oil", "BZ=F"); a.put("NATGAS", "HH=F");
         // Forex
-        a.put("USD", "UUP"); a.put("EURUSD", "EURUSD=X"); a.put("JPYUSD", "JPYUSD=X"); a.put("GBPUSD", "GBPUSD=X"); a.put("CHFUSD", "CHFUSD=X"); a.put("CZKUSD", "CZKUSD=X");
+        a.put("USD", "UUP"); a.put("EURUSD", "EURUSD=X"); a.put("JPYUSD", "JPYUSD=X"); 
+        a.put("GBPUSD", "GBPUSD=X"); a.put("CHFUSD", "CHFUSD=X");  a.put("CZKUSD", "CZKUSD=X");
         // Crypto
-        a.put("Bitcoin", "BTC-USD"); a.put("Ethereum", "ETH-USD"); a.put("Solana", "SOL-USD"); a.put("BNB", "BNB-USD"); a.put("XRP", "XRP-USD"); a.put("Cardano", "ADA-USD"); a.put("Avalanche", "AVAX-USD"); a.put("Polkadot", "DOT-USD"); a.put("Chainlink", "LINK-USD"); a.put("Litecoin", "LTC-USD"); a.put("Hype", "HYPE32196-USD"); a.put("Dogecoin", "DOGE-USD");
+        a.put("Bitcoin", "BTC-USD"); a.put("Ethereum", "ETH-USD"); a.put("Solana", "SOL-USD"); 
+        a.put("BNB", "BNB-USD"); a.put("XRP", "XRP-USD"); a.put("Cardano", "ADA-USD"); 
+        a.put("Avalanche", "AVAX-USD"); a.put("Polkadot", "DOT-USD"); a.put("Chainlink", "LINK-USD"); 
+        a.put("Litecoin", "LTC-USD"); a.put("Hype", "HYPE32196-USD"); a.put("Dogecoin", "DOGE-USD");
         // ETF
-        a.put("SP500", "SXR8.DE"); a.put("MSCIWorld", "EUNL.DE"); a.put("Nasdaq100", "CNDX.L"); a.put("DAX", "ES1.DE"); a.put("FTSE", "VWCE.DE");
+        a.put("SP500", "SXR8.DE"); a.put("MSCIWorld", "EUNL.DE"); a.put("Nasdaq100", "CNDX.L"); 
+        a.put("DAX", "ES1.DE"); a.put("FTSE", "VWCE.DE");
         // Tech
-        a.put("Palantir_Technologies", "PLTR"); a.put("Ericsson", "ERIC-B.ST"); a.put("Skoda_Doosan", "DSPW.PR"); a.put("TSM", "TSM"); a.put("MSFT", "MSFT"); a.put("Nokia", "NOKIA.HE"); a.put("GOOGL", "GOOGL"); a.put("Apple", "AAPL"); a.put("Tesla", "TSLA"); a.put("Netflix", "NFLX"); a.put("Colt", "CZG.PR"); a.put("CEZ", "CEZ.PR"); a.put("ORCL", "ORCL"); a.put("NVDA", "NVDA"); a.put("AMD", "AMD"); a.put("Adobe", "ADBE"); a.put("Intel", "INTC"); a.put("Spotify", "SPOT"); a.put("Coinbase", "COIN");
+        a.put("Palantir_Technologies", "PLTR"); a.put("Ericsson", "ERIC-B.ST"); a.put("Skoda_Doosan", "DSPW.PR"); 
+        a.put("TSM", "TSM"); a.put("MSFT", "MSFT"); a.put("Nokia", "NOKIA.HE"); a.put("GOOGL", "GOOGL"); 
+        a.put("Apple", "AAPL"); a.put("Tesla", "TSLA"); a.put("Netflix", "NFLX"); a.put("Colt", "CZG.PR"); 
+        a.put("CEZ", "CEZ.PR"); a.put("ORCL", "ORCL"); a.put("NVDA", "NVDA"); a.put("AMD", "AMD"); 
+        a.put("Adobe", "ADBE"); a.put("Intel", "INTC"); a.put("Spotify", "SPOT"); a.put("Coinbase", "COIN");
         // Defensive
-        a.put("Berkshire_Hathaway", "BRK-A"); a.put("Erste_Group_Bank_PR", "ERBAG.PR"); a.put("Coca-Cola", "KO"); a.put("CocaColaCCH", "CCH.L"); a.put("Altria", "MO"); a.put("Nestle", "NESN.SW"); a.put("AgnicoEagle", "AEM"); a.put("NewmontMining", "NEM"); a.put("NovoNordisk", "NOVO-B.CO"); a.put("Moneta", "MONET.PR"); a.put("KomBanka", "KOMB.PR"); a.put("UBS", "UBSG.SW"); a.put("Zurrich_Insurance", "ZURN.SW"); a.put("Nordea_Bank", "NDA-FI.HE"); a.put("British_American_Tobacco", "BTI"); a.put("Equinor", "EQNR"); a.put("Allianz", "ALV.DE"); a.put("Procter&Gamble", "PG");
+        a.put("Berkshire_Hathaway", "BRK-A"); a.put("Erste_Group_Bank_PR", "ERBAG.PR"); a.put("Coca-Cola", "KO"); 
+        a.put("CocaColaCCH", "CCH.L"); a.put("Altria", "MO"); a.put("Nestle", "NESN.SW"); 
+        a.put("AgnicoEagle", "AEM"); a.put("NewmontMining", "NEM"); a.put("NovoNordisk", "NOVO-B.CO"); 
+        a.put("Moneta", "MONET.PR"); a.put("KomBanka", "KOMB.PR"); a.put("UBS", "UBSG.SW"); a.put("Zurrich_Insurance", "ZURN.SW"); 
+        a.put("Nordea_Bank", "NDA-FI.HE"); a.put("British_American_Tobacco", "BTI"); a.put("Equinor", "EQNR"); 
+        a.put("Allianz", "ALV.DE"); a.put("Procter&Gamble", "PG");
         ASSETS = Collections.unmodifiableMap(a);
         // Profile assignments
         Map<String, String> p = new HashMap<>();
